2a1eb7f90508e5bbf6fc7edb8fde50b154684db8
Maximiliano Tulian <maxtulian93@gmail.com>
2023-10-17 16:50:27 -0300

    Add mocks

---
+++ src/mock/links.js
@@ -0,0 +1,39 @@
+const links = [
+  {
+    title: "Home",
+    url: "/Home",
+    iconCode: "home.png",
+  },
+  {
+    title: "Redirects",
+    url: "/redirect",
+    iconCode: "Redirects.png",
+  },
+  {
+    title: "URL shortener",
+    url: "/urlShortener",
+    iconCode: "link.png",
+  },
+  {
+    title: "Monitor",
+    url: "/monitor",
+    iconCode: "monitoring.png",
+  },
+  {
+    title: "Hostnames",
+    url: "/hostnames",
+    iconCode: "server.png",
+  },
+  {
+    title: "Subscriptions",
+    url: "/Subscriptions",
+    iconCode: "coin-stack.png",
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    iconCode: "settings.png",
+  }
+];
+
+export default links;
---
+++ src/mock/responseExample.js
@@ -0,0 +1,99 @@
+const responseExample = [
+  {
+      "id": "3adf89ef-0f7e-4804-85ca-aae0fe5894a1",
+      "hash": "vglvxl",
+      "name": "RedirHub",
+      "role": "owner",
+      "headurl": "https://www.urllize.com/avatar?r=w/c4ca4238a0b923820dcc509a6f75849b.png",
+      "personal": true,
+      "country": "CN",
+      "pm_type": null,
+      "pm_ending": null,
+      "level": 3,
+      "email": "",
+      "billing_extra": null,
+      "created_at": "2020-12-08T17:08:46.000000Z",
+      "updated_at": "2023-10-12T03:32:25.000000Z",
+      "meta": {
+          "cluster": "edge"
+      },
+      "summary": [
+          {
+              "category": "redirect",
+              "used": 140
+          },
+          {
+              "category": "short-url",
+              "used": 138
+          },
+          {
+              "category": "monitor",
+              "used": 9
+          },
+          {
+              "category": "members",
+              "used": 2
+          }
+      ]
+  },
+  {
+      "id": "25dcbd4d-08c9-4aba-b9e4-b04180511d29",
+      "hash": "lrazgq",
+      "name": "Brand new",
+      "role": "owner",
+      "headurl": "https://www.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e?s=128&d=mm&r=g&d=retro",
+      "personal": true,
+      "country": "CN",
+      "pm_type": null,
+      "pm_ending": null,
+      "level": 4,
+      "email": "",
+      "billing_extra": "",
+      "created_at": "2021-01-26T06:04:27.000000Z",
+      "updated_at": "2023-10-08T15:32:08.000000Z",
+      "meta": {
+          "cluster": "edge"
+      },
+      "summary": [
+          {
+              "category": "members",
+              "used": 1
+          }
+      ]
+  },
+  {
+      "id": "3013f486-ef6e-4b16-bcfa-aeb5638874d3",
+      "hash": "qevdwv",
+      "name": "Po",
+      "role": "owner",
+      "headurl": "https://www.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e?s=128&d=mm&r=g&d=retro",
+      "personal": false,
+      "country": "CN",
+      "pm_type": null,
+      "pm_ending": null,
+      "level": 3,
+      "email": "",
+      "billing_extra": "",
+      "created_at": "2023-10-09T11:31:12.000000Z",
+      "updated_at": "2023-10-10T05:31:24.000000Z",
+      "meta": {
+          "cluster": "edge3"
+      },
+      "summary": [
+          {
+              "category": "redirect",
+              "used": 1
+          },
+          {
+              "category": "short-url",
+              "used": 1
+          },
+          {
+              "category": "members",
+              "used": 1
+          }
+      ]
+  }
+];
+
+export default responseExample;
